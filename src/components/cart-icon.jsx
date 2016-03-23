de5fd6f9235495442e9ea79677c595485e559da3
Irene Robb <irenerobb72@gmail.com>
2016-03-23 15:01:35 -0400

    added cart icon

---
+++ src/components/cart-icon.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import reactMixin from 'react-mixin'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { connect } from 'react-redux'
+import * as actionCreators from '../action_creators'
+
+export class Action extends Component {
+  constructor(props) {
+    super(props)
+  }
+  handleClick(e) {
+    e.preventDefault()
+  }
+  render() {
+  }
+}
+
+reactMixin(Action.prototype, PureRenderMixin)
