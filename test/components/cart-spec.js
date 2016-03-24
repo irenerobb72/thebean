b44b05c51006604ccf07eda14a80c82887d39c19
Irene Robb <irenerobb72@gmail.com>
2016-03-23 20:41:26 -0400

    changed file for cart

---
+++ test/components/cart-spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import {Map} from 'immutable'
+
+chai.use(chaiEnzyme())
+
+import { mount, render, shallow } from 'enzyme'
+import {Action} from '../../src/components/Cart.jsx'
+
+describe('<Cart/>', () => {
+
+})
