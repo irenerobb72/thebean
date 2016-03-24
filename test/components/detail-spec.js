74d8700d2ecf567df29e314fc609d85848e1be5a
Irene Robb <irenerobb72@gmail.com>
2016-03-23 22:07:00 -0400

    added detail test

--- test/components/detail-spec.js
+++ test/components/detail-spec.js
@@ -9,5 +9,12 @@ import { mount, render, shallow } from 'enzyme'
 import {Action} from '../../src/components/Detail.jsx'
 
 describe('<Detail/>', () => {
-
+  it('renders without any props', () => {
+    const wrapper = shallow(<Detail/>)
+    expect(wrapper).to.be.ok
+  })
+  it('has a classname of carticon', () => {
+    const wrapper = shallow(<Detail/>)
+    expect(wrapper).to.have.className('Detail')
+  })
 })
