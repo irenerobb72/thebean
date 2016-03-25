78224bdcb46789bb70d8b32d18abe9eb2459e26c
Irene Robb <irenerobb72@gmail.com>
2016-03-25 14:12:46 -0400

    adding more to the reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,13 @@
 
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+      case 'BUY_ITEM':
+          return buy(state, action.task)
+      case 'CHECKOUT':
+        return checkout(state)
+    }
+    return state
+}
+
+export default reducer
