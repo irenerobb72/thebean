6b7d0b04eb4094b9546fb37c8d4beb100e67100a
Irene Robb <irenerobb72@gmail.com>
2016-03-25 14:08:26 -0400

    deleted the reducer.js stuff

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,1 @@
-import next from './reductions/next'
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'NEXT':
-      return next(state)
-  }
-  return state
-}
-
-export default reducer
