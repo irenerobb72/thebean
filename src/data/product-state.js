dc980dbd877c8a4fbb23165f6c3a0379ee748952
Irene Robb <irenerobb72@gmail.com>
2016-03-25 22:18:26 -0400

    added product reducer

---
+++ src/data/product-state.js
@@ -0,0 +1,10 @@
+import { fromJS } from 'immutable'
+
+const products = fromJS{[
+  products: [
+    {id: 1, name:'Case', price:15, qty:0},
+    {id: 2, name:'Glasses', price:20, qty:0},
+    {id: 3, name:'Shirt', price:40, qty:0},
+    {id: 4, name:'Shoe', price:100, qty:0},
+  ]
+})
