0b2ab4e1b2ab2993530905d0e227eb7838f66ff7
Irene Robb <irenerobb72@gmail.com>
2016-03-26 20:41:28 -0400

    add-to cart describe

--- test/reducer/add-to-cart-spec.js
+++ test/reducer/add-to-cart-spec.js
@@ -1,3 +1,11 @@
 import { expect } from 'chai'
 import { List, Map, fromJS } from 'immutable'
 import { addToCart } from '../src/reductions/addToCart.js'
+
+describe('BUY_ITEM', () => {
+  let initialState = sourceState
+  const activity = {
+    type: 'BUY-TIME',
+    item: 'glasses'
+  }
+})
