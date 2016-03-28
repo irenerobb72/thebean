93b3a812c167480ebd89c2e5aad30e57b7197f2b
Irene Robb <irenerobb72@gmail.com>
2016-03-28 16:22:03 -0400

    added containers

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import reactMixin from 'react-mixin'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { connect } from 'react-redux'
-import { Cart } from '/Cart.jsx'
-import { Carticon } from '/Carticon.jsx'
-import { Detail } from '/Detail.jsx'
-import { Products } from '/Products.jsx'
+import { Cartcontainer } from '/Cart.jsx'
+import { CarticonContainer } from '/Carticon.jsx'
+import { Detailcontainer } from '/Detail.jsx'
+import { Productscontainer } from '/Products.jsx'
 
 export class App extends Component {
   constructor(props) {
@@ -13,7 +13,9 @@ export class App extends Component {
   }
 
   render() {
-    <div className="app"></div>
+    <div className="app">
+
+    </div>
 
   }
 }
