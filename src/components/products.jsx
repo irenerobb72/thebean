5402b573054a98dd83a6a206e3ce6a54ae1a0a2e
Irene Robb <irenerobb72@gmail.com>
2016-03-28 09:56:45 -0400

    map state to props function

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -3,21 +3,30 @@ import reactMixin from 'react-mixin'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { connect } from 'react-redux'
 import * as actionCreators from '../action_creators'
+import products from './data/products-state.js'
 
 export class Products extends Component {
   constructor(props) {
     super(props)
   }
   render() {
-    const products =
-    return (
-      <div className="products">
-        <div>
-          <h1>The Bean</h1>
-        </div>
-        { products }
+    const products = this.props.products.map( product => {
+      return <Products key={product.get('id')} product={product} />
+  })
+  return (
+    <div className="products">
+      <div>
+        <h1>The Bean</h1>
       </div>
+        { products }
+    </div>
     )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    products: state.get('products')
   }
 }
 
