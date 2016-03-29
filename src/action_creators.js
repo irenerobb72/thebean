d1579c9b96bbaeb10cd271d4b116e97fde79473d
Irene Robb <irenerobb72@gmail.com>
2016-03-29 11:11:48 -0400

    fixed the action creators

--- src/action_creators.js
+++ src/action_creators.js
@@ -1,5 +1,20 @@
-export function next() {
+export function buyItem() {
   return {
-    type: 'NEXT'
+    type: 'BUY_ITEM',
+    product: product
+  }
+}
+
+export function addToCart() {
+  return {
+    type: 'ADD_TO_CART',
+    product: product
+  }
+}
+
+export function removeFromCart() {
+  return {
+    type: 'REMOVE_FROM_CART',
+    product: product
   }
 }
