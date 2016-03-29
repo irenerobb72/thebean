68840da2c1224bd904231275c9fc6aa9ed06a6fd
Irene Robb <irenerobb72@gmail.com>
2016-03-29 10:58:27 -0400

    redid buy item

--- src/reductions/buyitem.js
+++ src/reductions/buyitem.js
@@ -1,26 +1,11 @@
 var card = '4563960122001999'
 
-function buyItem(state, item) {
-  const Id = state.get('items').findIndex(
-    (index) => index.get('name') === item)
+function buyItem() {
   return validateCard () ?
-  canBuy(state, item, itemId) : state
 }
 
 function validateCard(card) {
-  var total = 0
-  var splitTheNumber = card.split('')
-  var mapNumber = splitTheNumber.map(Number)
-  var numberThis = mapNumber.map((number, index) => {
-  if (index % 2 === 0) {
-    return parseInt(number) * 2
-  } else {
-    return parseInt(number)
-    }
-  })
-  var joinNumber = numberThis.join('').split('')
-  total = joinNumber.reduce((a, b) => a * 1 + b * 1)
-  return total % 10 === 0
+
 }
 
 function canBuy (state, item, itemId) {
