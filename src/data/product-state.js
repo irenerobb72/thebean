0fb4d5042b539c58caa40845ef0ab48453f8984d
Irene Robb <irenerobb72@gmail.com>
2016-03-30 18:37:54 -0400

    fixed the from js to map

--- src/data/product-state.js
+++ src/data/product-state.js
@@ -1,12 +1,34 @@
-import { fromJS } from 'immutable'
+import { Map, List } from 'immutable'
 
-const products = fromJS{[
-  products: [
-    {id: 1, name:'Case', price:15, qty:0},
-    {id: 2, name:'Glasses', price:20, qty:0},
-    {id: 3, name:'Shirt', price:40, qty:0},
-    {id: 4, name:'Shoe', price:100, qty:0},
-  ]
-})
-
+const products = List([
+  Map({
+    id: 1,
+    name: 'Case',
+    money: 15,
+    qty: 0
+  }),
+  Map({
+    id: 2,
+    name: 'Glasses',
+    price: 20,
+    qty: 0
+  }),
+  Map({
+    id: 3,
+    name: 'Shirt',
+    price: 40, 
+    qty: 0
+  }),
+  Map({
+    id: 4,
+    name: 'Shoe',
+    price: 100,
+    qty: 0
+  })
+)
 export default products
+
+{id: 1, name:'Case', price:15, qty:0},
+{id: 2, name:'Glasses', price:20, qty:0},
+{id: 3, name:'Shirt', price:40, qty:0},
+{id: 4, name:'Shoe', price:100, qty:0},
