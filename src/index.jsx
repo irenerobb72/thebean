b25df45e4e3295e8ba7b222715ab2c349fc20710
Irene Robb <irenerobb72@gmail.com>
2016-03-31 20:18:23 -0400

    deleted things i dont need

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import Router, { Route } from 'react-router'
 import { App } from './components/App.jsx'
 import reducer from './reducer.js'
 
