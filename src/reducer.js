cc96b023e79e9f83512c59ccfca13715265aa9b9
Irene Robb <irenerobb72@gmail.com>
2016-04-21 22:57:02 -0400

    added books to the store

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,11 @@ import {fromJS} from 'immutable'
 
 const INITIAL_STATE = fromJS({
   products: [
+    {id: 1, name:'The Glass Castle', price: 10, qty: 1},
+    {id: 2, name:'Dune', price: 15, qty: 1},
+    {id: 3, name:'Memoris of a Geisha', price: 9, qty: 1},
+    {id: 4, name:'1984', price: 7, qty: 1},
+    {id: 5, name:'Stranger in a Strange Land', price: 12, qty: 1}
   ]
 })
 
