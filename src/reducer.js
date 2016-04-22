4b14e24ad4419d6f929f4bdf5f8bbbc176ab82d4
Irene Robb <irenerobb72@gmail.com>
2016-04-21 23:01:12 -0400

    added authors

--- src/reducer.js
+++ src/reducer.js
@@ -2,12 +2,13 @@ import {fromJS} from 'immutable'
 
 const INITIAL_STATE = fromJS({
   products: [
-    {id: 1, name:'The Glass Castle', price: 10, qty: 1},
-    {id: 2, name:'Dune', price: 15, qty: 1},
-    {id: 3, name:'Memoris of a Geisha', price: 9, qty: 1},
-    {id: 4, name:'1984', price: 7, qty: 1},
-    {id: 5, name:'Stranger in a Strange Land', price: 12, qty: 1}
-  ]
+    {id:1, name:'The Glass Castle', author:'Jeannette Walls', price:10, qty:1},
+    {id:2, name:'Dune', author:'Frank Herbert', price:15, qty:1},
+    {id:3, name:'Memoris of a Geisha', author:'Arthur Golden', price:9, qty:1},
+    {id:4, name:'1984', author:'George Orwell', price:7, qty:1},
+    {id:5, name:'Stranger in a Strange Land', author:'Robert A. Heinlein', price:12, qty:1}
+  ],
+  cart: [1,2]
 })
 
 export default (state = INITIAL_STATE, action) => {
