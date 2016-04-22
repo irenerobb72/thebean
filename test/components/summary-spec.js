325a227a650f1a26b13e94fccfa7c9d2660c1be6
Irene Robb <irenerobb72@gmail.com>
2016-04-21 23:58:43 -0400

    added summary tests

---
+++ test/components/summary-spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import {Map} from 'immutable'
+
+chai.use(chaiEnzyme())
+
+import { mount, render, shallow } from 'enzyme'
+import {Checkout} from '../../src/components/Summary.jsx'
+
+describe('<Summary/>', () => {
+  it('renders without props', () => {
+    const wrapper = shallow(<Summary/>)
+    expect(wrapper).to.be.ok
+  })
+  it('has a classname of summary', () => {
+    const wrapper = shallow(<Summary/>)
+    expect(wrapper).to.have.className('summary')
+  })
+})
