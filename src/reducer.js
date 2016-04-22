b72b8eb7bedadc5d1031695bdcaf8b41e4bc75d5
Irene Robb <irenerobb72@gmail.com>
2016-04-21 22:52:30 -0400

    got rid of something that doesnt make sense

--- src/reducer.js
+++ src/reducer.js
@@ -8,7 +8,7 @@ const INITIAL_STATE = fromJS({
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'REMOVE_PRODUCT_FROM_CART':
-      return state.set('cart', state.get('cart').slice(1, index).concat(action.slice[index]))
+      return state
     default:
       return state
   }
