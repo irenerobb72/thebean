aeb6a9b466e2dd742e11370c8e3068232d1ebce2
Irene Robb <irenerobb72@gmail.com>
2016-04-24 13:05:25 -0400

    const products

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -2,10 +2,10 @@ import React, {Component} from 'react'
 
 export default class Summary extends Component {
   render() {
-
+    const products
     return (
       <div className='summary'>
-        
+
       </div>
     )
   }
